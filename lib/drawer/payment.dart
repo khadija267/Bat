2a30b92729346cf34f88a47fc4415e1ad0372127
khadija267 <khadiga267@gmail.com>
2020-07-30 03:35:24 +0200

    edit

--- lib/drawer/payment.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-class Payment{
-  // addCard(token){
-  //   FirebaseAuth.instance.currentUser().then(user){
-      
-  //   }
-  // }
-}
